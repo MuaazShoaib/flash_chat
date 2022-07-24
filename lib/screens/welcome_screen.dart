41a9896d923b9d60d00c72b6772e42756fbb6094
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-07-24 09:54:18 +0500

    174. Custom Flutter Animations with the Animation Controller - Completed

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -9,11 +9,61 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      // duration: Duration(seconds: 3),
+      duration: Duration(seconds: 1),
+      vsync: this,
+      // upperBound: 100.0,
+      // upperBound: 1,
+    );
+
+    // animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+
+    animation = ColorTween(
+      // begin: Colors.red,
+      begin: Colors.blueGrey,
+      // end: Colors.blue,
+      end: Colors.white,
+    ).animate(controller);
+
+    controller.forward();
+    // controller.reverse(from: 1.0);
+
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    // });
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.red.withOpacity(controller.value),
+      // backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -26,14 +76,17 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
+                    // height: controller.value,
+                    // height: animation.value * 100,
                     height: 60.0,
                   ),
                 ),
                 Text(
+                  // '${controller.value.toInt()} %',
                   'Flash Chat',
                   style: TextStyle(
                     fontSize: 45.0,
-                    // fontWeight: FontWeight.w900,
+                    fontWeight: FontWeight.w900,
                   ),
                 ),
               ],
